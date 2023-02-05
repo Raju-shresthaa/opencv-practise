1288af8f7bb2e1bab36887e5aba8c716dce8ead1
Raju <rajusth567@gmail.com>
2023-02-05 12:23:54 +0545

    [basic feature][show,rotate,resize]

--- basic.py
+++ basic.py
@@ -1,5 +1,15 @@
 import cv2 as cv
+import cv2.cv2
+import random
 
-img = cv.imread('Photos/cat.jpg')
+img = cv.imread('Photos/lady.jpg')
+rotate_img = cv.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+cv.imshow("rotate_image", rotate_img)
+resize_img = cv.resize(img, (0, 0), fx=2, fy=2)
+cv.imshow("resize_image", resize_img)
 cv.imshow("Cat Output", img)
-cv.waitKey(5000)
+cv.imwrite("rotated.jpg", rotate_img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
